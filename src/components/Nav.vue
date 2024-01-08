0eea6bd1b484f17869a2ab2658995b4a4d9874fd
matt12171 <matthewpatterson@hotmail.co.uk>
2024-01-08 12:14:39 +0000

    Sign in changes

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -5,14 +5,7 @@
   
         <div v-if="isLogin" class="login">
           <Avatar :src="user.photoURL" />
-          <button class="text-gray-400 hover:text-white" @click="signOut">
-            Sign Out
-          </button>
-        </div>
-  
-        <button v-else class="bg-green-500 hover:bg-green-600" @click="signIn">
-          Sign in
-        </button>
+        </div>    
       </div>
     </nav>
   </template>
